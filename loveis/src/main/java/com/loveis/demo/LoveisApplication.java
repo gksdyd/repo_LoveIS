a2592ffd8e2044961cd26ab26d990e6aa1ef7377
gksdyd <gksdyd009@naver.com>
2025-04-23 17:13:13 +0900

    1. git Init

---
+++ loveis/src/main/java/com/loveis/demo/LoveisApplication.java
@@ -0,0 +1,13 @@
+package com.loveis.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoveisApplication.class, args);
+	}
+
+}
---
+++ loveis/src/test/java/com/loveis/demo/LoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loveis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
