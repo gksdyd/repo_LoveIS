83065833973e1ea84af65c73fe3eb6704a8093cb
gksdyd <gksdyd009@naver.com>
2025-04-30 18:16:18 +0900

    1. cache에 code 값 저장하는 소스 주석 제거 2. 멤버리스트 목록 표시되도록 수정 3. db 멤버 컬럼에 따른 dto 수정

--- loveis/src/main/java/com/loveis/demo/module/member/MemberDto.java
+++ loveis/src/main/java/com/loveis/demo/module/member/MemberDto.java
@@ -9,35 +9,40 @@ public class MemberDto {
 	private String userPassword;
 	private String userBirth;	
 	private String userEmail;	
-	private String userAgency;	
-	private String userPhone;	
-	private int userRecvNotiFlag;	
-	private String userPostCode;	
-	private String userAddr;	
-	private String userJibunAddr;	
-	private String userDetailAddr;	
-	private String userReference;	
-	private double userLatitude;	
-	private double userLongitude;	
+	private String userPhone;
+	private String userCity;
+	private String userLocal;
 	private int userDelFlag;	
-	private int userSavePoint;
+	private int userPeanut;
 	private String userRegDate;
 	private String userModDate;
-	private int petCount;
-	private int userSecession;
+	private Double userHeight;
+	private Double userWeight;
+	private String userIntroduce;
+	private Integer userEducation;
+	private Integer userMBTI;
+	private Integer userSmoking;
+	private Integer userPetNy;
+	private Integer userDatingStyle;
+	private String userJob;
+	private Integer userStyle;
+	private Integer userFirstFace;
+	private Integer userChurch;
+	private Integer userAlcohol;
+	
+	private String persSeq;
+	private Integer persText;
+	private Integer personalCount;
+	
+	private String inteSeq;
+	private Integer inteText;
+	private Integer interestCount;
+	
+	private String hobbSeq;
+	private Integer hobbText;
+	private Integer hobbyCount;
 	
 	private String newPassword;
-	private String userEtcReason;
-	
-	private Integer dateDiff;
-	
-	private String prodSeq;
-	private String reviSeq;
-	private Integer reviScore;
-	private String reviText;
-	private String reviRegDate;
-	private String reviModDate;
-	private Integer reviGood;
 	
 	public String getUserSeq() {
 		return userSeq;
@@ -81,53 +86,11 @@ public class MemberDto {
 	public void setUserPhone(String userPhone) {
 		this.userPhone = userPhone;
 	}
-	public int getUserRecvNotiFlag() {
-		return userRecvNotiFlag;
-	}
-	public void setUserRecvNotiFlag(int userRecvNotiFlag) {
-		this.userRecvNotiFlag = userRecvNotiFlag;
-	}
-	public String getUserPostCode() {
-		return userPostCode;
-	}
-	public void setUserPostCode(String userPostCode) {
-		this.userPostCode = userPostCode;
-	}
-	public String getUserAddr() {
-		return userAddr;
-	}
-	public void setUserAddr(String userAddr) {
-		this.userAddr = userAddr;
-	}
-	public String getUserDetailAddr() {
-		return userDetailAddr;
-	}
-	public void setUserDetailAddr(String userDetailAddr) {
-		this.userDetailAddr = userDetailAddr;
-	}
-	public double getUserLatitude() {
-		return userLatitude;
-	}
-	public void setUserLatitude(double userLatitude) {
-		this.userLatitude = userLatitude;
-	}
-	public double getUserLongitude() {
-		return userLongitude;
-	}
-	public void setUserLongitude(double userLongitude) {
-		this.userLongitude = userLongitude;
-	}
 	public int getUserDelFlag() {
 		return userDelFlag;
 	}
 	public void setUserDelFlag(int userDelFlag) {
 		this.userDelFlag = userDelFlag;
-	}
-	public int getUserSavePoint() {
-		return userSavePoint;
-	}
-	public void setUserSavePoint(int userSavePoint) {
-		this.userSavePoint = userSavePoint;
 	}
 	public String getUserRegDate() {
 		return userRegDate;
@@ -141,23 +104,11 @@ public class MemberDto {
 	public void setUserModDate(String userModDate) {
 		this.userModDate = userModDate;
 	}
-	public int getPetCount() {
-		return petCount;
-	}
-	public void setPetCount(int petCount) {
-		this.petCount = petCount;
-	}
 	public String getUserPassword() {
 		return userPassword;
 	}
 	public void setUserPassword(String userPassword) {
 		this.userPassword = userPassword;
-	}
-	public String getUserAgency() {
-		return userAgency;
-	}
-	public void setUserAgency(String userAgency) {
-		this.userAgency = userAgency;
 	}
 	public String getNewPassword() {
 		return newPassword;
@@ -165,76 +116,154 @@ public class MemberDto {
 	public void setNewPassword(String newPassword) {
 		this.newPassword = newPassword;
 	}
-	public int getUserSecession() {
-		return userSecession;
+	public String getUserCity() {
+		return userCity;
 	}
-	public void setUserSecession(int userSecession) {
-		this.userSecession = userSecession;
+	public void setUserCity(String userCity) {
+		this.userCity = userCity;
 	}
-	public String getUserEtcReason() {
-		return userEtcReason;
+	public String getUserLocal() {
+		return userLocal;
 	}
-	public void setUserEtcReason(String userEtcReason) {
-		this.userEtcReason = userEtcReason;
+	public void setUserLocal(String userLocal) {
+		this.userLocal = userLocal;
 	}
-	public Integer getDateDiff() {
-		return dateDiff;
+	public int getUserPeanut() {
+		return userPeanut;
 	}
-	public void setDateDiff(Integer dateDiff) {
-		this.dateDiff = dateDiff;
+	public void setUserPeanut(int userPeanut) {
+		this.userPeanut = userPeanut;
 	}
-	public String getUserJibunAddr() {
-		return userJibunAddr;
+	public Double getUserHeight() {
+		return userHeight;
 	}
-	public void setUserJibunAddr(String userJibunAddr) {
-		this.userJibunAddr = userJibunAddr;
+	public void setUserHeight(Double userHeight) {
+		this.userHeight = userHeight;
 	}
-	public String getUserReference() {
-		return userReference;
+	public Double getUserWeight() {
+		return userWeight;
 	}
-	public void setUserReference(String userReference) {
-		this.userReference = userReference;
+	public void setUserWeight(Double userWeight) {
+		this.userWeight = userWeight;
 	}
-	public String getProdSeq() {
-		return prodSeq;
+	public String getUserIntroduce() {
+		return userIntroduce;
 	}
-	public void setProdSeq(String prodSeq) {
-		this.prodSeq = prodSeq;
+	public void setUserIntroduce(String userIntroduce) {
+		this.userIntroduce = userIntroduce;
 	}
-	public Integer getReviScore() {
-		return reviScore;
+	public Integer getUserEducation() {
+		return userEducation;
 	}
-	public void setReviScore(Integer reviScore) {
-		this.reviScore = reviScore;
+	public void setUserEducation(Integer userEducation) {
+		this.userEducation = userEducation;
 	}
-	public String getReviText() {
-		return reviText;
+	public Integer getUserMBTI() {
+		return userMBTI;
 	}
-	public void setReviText(String reviText) {
-		this.reviText = reviText;
+	public void setUserMBTI(Integer userMBTI) {
+		this.userMBTI = userMBTI;
 	}
-	public String getReviRegDate() {
-		return reviRegDate;
+	public Integer getUserSmoking() {
+		return userSmoking;
 	}
-	public void setReviRegDate(String reviRegDate) {
-		this.reviRegDate = reviRegDate;
+	public void setUserSmoking(Integer userSmoking) {
+		this.userSmoking = userSmoking;
 	}
-	public String getReviModDate() {
-		return reviModDate;
+	public Integer getUserPetNy() {
+		return userPetNy;
 	}
-	public void setReviModDate(String reviModDate) {
-		this.reviModDate = reviModDate;
+	public void setUserPetNy(Integer userPetNy) {
+		this.userPetNy = userPetNy;
 	}
-	public Integer getReviGood() {
-		return reviGood;
+	public Integer getUserDatingStyle() {
+		return userDatingStyle;
 	}
-	public void setReviGood(Integer reviGood) {
-		this.reviGood = reviGood;
+	public void setUserDatingStyle(Integer userDatingStyle) {
+		this.userDatingStyle = userDatingStyle;
 	}
-	public String getReviSeq() {
-		return reviSeq;
+	public String getUserJob() {
+		return userJob;
 	}
-	public void setReviSeq(String reviSeq) {
-		this.reviSeq = reviSeq;
+	public void setUserJob(String userJob) {
+		this.userJob = userJob;
+	}
+	public Integer getUserStyle() {
+		return userStyle;
+	}
+	public void setUserStyle(Integer userStyle) {
+		this.userStyle = userStyle;
+	}
+	public Integer getUserFirstFace() {
+		return userFirstFace;
+	}
+	public void setUserFirstFace(Integer userFirstFace) {
+		this.userFirstFace = userFirstFace;
+	}
+	public Integer getUserChurch() {
+		return userChurch;
+	}
+	public void setUserChurch(Integer userChurch) {
+		this.userChurch = userChurch;
+	}
+	public Integer getUserAlcohol() {
+		return userAlcohol;
+	}
+	public void setUserAlcohol(Integer userAlcohol) {
+		this.userAlcohol = userAlcohol;
+	}
+	public String getPersSeq() {
+		return persSeq;
+	}
+	public void setPersSeq(String persSeq) {
+		this.persSeq = persSeq;
+	}
+	public Integer getPersText() {
+		return persText;
+	}
+	public void setPersText(Integer persText) {
+		this.persText = persText;
+	}
+	public String getInteSeq() {
+		return inteSeq;
+	}
+	public void setInteSeq(String inteSeq) {
+		this.inteSeq = inteSeq;
+	}
+	public Integer getInteText() {
+		return inteText;
+	}
+	public void setInteText(Integer inteText) {
+		this.inteText = inteText;
+	}
+	public String getHobbSeq() {
+		return hobbSeq;
+	}
+	public void setHobbSeq(String hobbSeq) {
+		this.hobbSeq = hobbSeq;
+	}
+	public Integer getHobbText() {
+		return hobbText;
+	}
+	public void setHobbText(Integer hobbText) {
+		this.hobbText = hobbText;
+	}
+	public Integer getPersonalCount() {
+		return personalCount;
+	}
+	public void setPersonalCount(Integer personalCount) {
+		this.personalCount = personalCount;
+	}
+	public Integer getInterestCount() {
+		return interestCount;
+	}
+	public void setInterestCount(Integer interestCount) {
+		this.interestCount = interestCount;
+	}
+	public Integer getHobbyCount() {
+		return hobbyCount;
+	}
+	public void setHobbyCount(Integer hobbyCount) {
+		this.hobbyCount = hobbyCount;
 	}
 }
