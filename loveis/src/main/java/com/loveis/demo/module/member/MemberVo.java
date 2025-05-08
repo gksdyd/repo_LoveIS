23bde9cc85b49f5489072f1188567b956ce65b62
gksdyd <gksdyd009@naver.com>
2025-05-08 20:37:46 +0900

    1. memberList filter 완료

--- loveis/src/main/java/com/loveis/demo/module/member/MemberVo.java
+++ loveis/src/main/java/com/loveis/demo/module/member/MemberVo.java
@@ -5,6 +5,21 @@ import com.loveis.demo.module.base.BaseVo;
 public class MemberVo extends BaseVo {
 
 	private Integer shSecession;
+	
+	private Integer shGender;
+	private Integer shAge;
+	private Integer shDistance;
+	private Integer shWeight;
+	private Integer shStyle;
+	private Integer shFirstFace;
+	private Integer shPersonality;
+	private Integer shDatingStyle;
+	private Integer shMbti;
+	private Integer shSmoking;
+	private Integer shHobby;
+	private Integer shPet;
+	private Integer shReligion;
+	private Integer shDrink;
 
 	public Integer getShSecession() {
 		return shSecession;
@@ -14,6 +29,118 @@ public class MemberVo extends BaseVo {
 		this.shSecession = shSecession;
 	}
 	
+	public Integer getShGender() {
+		return shGender;
+	}
+
+	public void setShGender(Integer shGender) {
+		this.shGender = shGender;
+	}
+
+	public Integer getShAge() {
+		return shAge;
+	}
+
+	public void setShAge(Integer shAge) {
+		this.shAge = shAge;
+	}
+
+	public Integer getShDistance() {
+		return shDistance;
+	}
+
+	public void setShDistance(Integer shDistance) {
+		this.shDistance = shDistance;
+	}
+
+	public Integer getShWeight() {
+		return shWeight;
+	}
+
+	public void setShWeight(Integer shWeight) {
+		this.shWeight = shWeight;
+	}
+
+	public Integer getShStyle() {
+		return shStyle;
+	}
+
+	public void setShStyle(Integer shStyle) {
+		this.shStyle = shStyle;
+	}
+
+	public Integer getShFirstFace() {
+		return shFirstFace;
+	}
+
+	public void setShFirstFace(Integer shFirstFace) {
+		this.shFirstFace = shFirstFace;
+	}
+
+	public Integer getShPersonality() {
+		return shPersonality;
+	}
+
+	public void setShPersonality(Integer shPersonality) {
+		this.shPersonality = shPersonality;
+	}
+
+	public Integer getShDatingStyle() {
+		return shDatingStyle;
+	}
+
+	public void setShDatingStyle(Integer shDatingStyle) {
+		this.shDatingStyle = shDatingStyle;
+	}
+
+	public Integer getShMbti() {
+		return shMbti;
+	}
+
+	public void setShMbti(Integer shMbti) {
+		this.shMbti = shMbti;
+	}
+
+	public Integer getShSmoking() {
+		return shSmoking;
+	}
+
+	public void setShSmoking(Integer shSmoking) {
+		this.shSmoking = shSmoking;
+	}
+
+	public Integer getShHobby() {
+		return shHobby;
+	}
+
+	public void setShHobby(Integer shHobby) {
+		this.shHobby = shHobby;
+	}
+
+	public Integer getShPet() {
+		return shPet;
+	}
+
+	public void setShPet(Integer shPet) {
+		this.shPet = shPet;
+	}
+
+	public Integer getShReligion() {
+		return shReligion;
+	}
+
+	public void setShReligion(Integer shReligion) {
+		this.shReligion = shReligion;
+	}
+
+	public Integer getShDrink() {
+		return shDrink;
+	}
+
+	public void setShDrink(Integer shDrink) {
+		this.shDrink = shDrink;
+	}
+
 	public String ranCertification() {
 		String rt = "";
 		for (int i = 0; i < 6; i++) {
