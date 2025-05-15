2c7700e480fb46be0104e6215f9092548352d5ba
gksdyd <gksdyd009@naver.com>
2025-05-15 17:57:19 +0900

    Revert "PayLove 붙임 수정"
    This reverts commit d62bfd3f985612420a0b265ff672b8f2a2368ad9.

--- loveis/src/main/java/com/loveis/demo/module/payment/PayController.java
+++ loveis/src/main/java/com/loveis/demo/module/payment/PayController.java
@@ -16,12 +16,12 @@ public class PayController {
 		return "love/payment/PayLoveList";
 	}
 	
-	@RequestMapping(value = "/PayLoveSuccess")
+	@RequestMapping(value = "/Success")
 	public String paysuccess() {
 		return "love/payment/TossPaymentsSucess";
 	}
 	
-	@RequestMapping(value = "/PayLoveFail")
+	@RequestMapping(value = "/Fail")
 	public String payfail() {
 		return "love/payment/TossPaymentsFail";
 	}
