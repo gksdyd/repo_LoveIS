a8c7044f776b69f2a0aa94a9583dcf31384109ff
gksdyd <gksdyd009@naver.com>
2025-05-20 17:17:42 +0900

    1. AI 애정도 기능 완료

--- loveis/src/main/java/com/loveis/demo/module/ai/AiController.java
+++ loveis/src/main/java/com/loveis/demo/module/ai/AiController.java
@@ -1,16 +1,30 @@
 package com.loveis.demo.module.ai;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.loveis.demo.module.base.BaseController;
+import com.loveis.demo.module.code.CodeDto;
+import com.loveis.demo.module.member.MemberService;
+import com.loveis.demo.module.member.MemberVo;
+
+import jakarta.servlet.http.HttpSession;
 
 
 @Controller
 @RequestMapping(value = "love/ai")
-public class AiController {
+public class AiController extends BaseController {
+	
+	@Autowired
+	MemberService memberService;
 	
 	@RequestMapping(value = "/AiLoveList")
-	public String codeXdmList() {
-
+	public String aiLoveList(MemberVo vo, Model model, HttpSession httpSession) {
+		vo.setUserSeq(httpSession.getAttribute("sessSeqUser").toString());
+		model.addAttribute("item", memberService.selectOne(vo));
+		model.addAttribute("code", CodeDto.cachedCodeArrayList);
 		return "/love/ai/AiLoveList";
-	}
+	}	
 }
