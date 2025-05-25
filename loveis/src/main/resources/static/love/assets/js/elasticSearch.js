96a74fab061c89379a66efcc50e19dd501ac70c1
gksdyd <gksdyd009@naver.com>
2025-05-26 00:25:31 +0900

    1. elastic search 사용하여 검색기능 구현 2. 메뉴탭만 서치되도록 구현

---
+++ loveis/src/main/resources/static/love/assets/js/elasticSearch.js
@@ -0,0 +1,42 @@
+const ENGLISH = /[a-zA-Z]/;
+const KOREAN = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+
+function elasticSearch() {
+    $("#elasticSearchList").empty();
+    const inputVal = $("#elasticSearch").val();
+
+    if (inputVal === "") {
+        return;
+    }
+
+    let text;
+    if (ENGLISH.test(inputVal)) {
+        let temp = inputVal.toUpperCase();
+        text = `SELECT engName, url FROM loveis WHERE engName LIKE '%${temp}%'`;
+    } else if (KOREAN.test(inputVal)) {
+        text = `SELECT name, url FROM loveis WHERE name LIKE '%${inputVal}%'`;
+    } else {
+        return;
+    }
+
+    const requestBody = {
+        query: text
+    };
+
+    fetch("http://localhost:9200/_sql?format=json&pretty", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(requestBody)
+    })
+    .then(res => res.json())
+    .then(data => {
+        for (let i = 0; i < data.rows.length; i++) {
+            let list = "<li class='searchList'><a href=" + data.rows[i][1] + ">" + data.rows[i][0] + "</a></li>";
+            $("#elasticSearchList").append(list);
+        }
+        console.log(data);
+        // alert(data.rows.length);
+    });
+}
