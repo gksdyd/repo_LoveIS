114ceb000927e760f783d1fc1ead38f257b16500
gksdyd <gksdyd009@naver.com>
2025-05-31 13:03:40 +0900

    1. 배포 주소로 변경

--- loveis/src/main/java/com/loveis/demo/module/base/Constants.java
+++ loveis/src/main/java/com/loveis/demo/module/base/Constants.java
@@ -8,5 +8,6 @@ public class Constants {
 	
 	public static final String TIME_FORMAT_HHMMSS = "HH:mm:ss";
 	
-	public static final String LOCAL_ADDRESS = "http://localhost:9200/";
+//	public static final String LOCAL_ADDRESS = "http://localhost:9200/";
+	public static final String LOCAL_ADDRESS = "http://43.201.16.176:9200/";
 }
